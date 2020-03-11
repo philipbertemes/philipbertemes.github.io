5f5ef5bcd363b618ac180b008d2e0a32e475f741
philipbertemes <philipbertemes64@gmail.com>
2020-03-11 11:07:04 +0100

    particlesjs changed to DNA strands

--- src/js/original_app.js
+++ src/js/original_app.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "value": "#ffffff"
       },
       "shape": {
-        "type": "image",
+        "type": "circle",
         "stroke": {
           "width": 0,
           "color": "#000000"
@@ -24,17 +24,11 @@ document.addEventListener("DOMContentLoaded", function () {
         "polygon": {
           "nb_sides": 5
         },
-        "images": [{
-          "src": "assets/letter_A.png",
-          "width": 100,
-          "height": 100
-        },
-        {
-          "src": "assets/letter_G.png",
+        "image": {
+          "src": "img/github.svg",
           "width": 100,
           "height": 100
         }
-        ]
       },
       "opacity": {
         "value": 1,
